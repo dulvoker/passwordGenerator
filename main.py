b0b340b94a0ebdb1ab4444088e19dc8effa6b428
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-10-20 03:59:48 +0600

    values to get for the password are ready

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+uppercase_letters = str()
+for i in range(65, 91) : uppercase_letters += chr(i)
+lowercase_letters = uppercase_letters.lower()
+digits = list()
+for i in range(10): digits.append(i)
+
