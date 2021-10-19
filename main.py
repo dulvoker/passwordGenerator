3f788af42be39c3df079072c6eb7e93b51b803bb
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-10-20 04:08:05 +0600

    finished the project

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ uppercase_letters = str()
 for i in range(65, 91) : uppercase_letters += chr(i)
 lowercase_letters = uppercase_letters.lower()
 digits = str()
-for i in range(10): digits += i
+for i in range(10): digits += str(i)
 symbols = "(){}[]!@#$%^&*\\\'\">?<,."
 value_for_password = str()
 
-upper, lower, nums, symbs = True, True, True, True
+upper, lower, nums, symbs = True, True, True, False
 if upper: value_for_password += uppercase_letters
 if lower: value_for_password += lowercase_letters
 if nums: value_for_password += digits
 if symbs: value_for_password += symbols
+
+length = 15
+password_amount = 10
+
+for i in range(password_amount):
+    password = "".join(random.sample(value_for_password, length))
+    print(password)
 
 
 
