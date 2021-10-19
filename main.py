c4fda288ef1e7de590705c88ae758030ea0af434
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-10-20 04:06:01 +0600

    seperated characters and added them to the value_for_passwords if this kind of password character is acceptable

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import random
 uppercase_letters = str()
 for i in range(65, 91) : uppercase_letters += chr(i)
 lowercase_letters = uppercase_letters.lower()
-digits = list()
-for i in range(10): digits.append(i)
+digits = str()
+for i in range(10): digits += i
 symbols = "(){}[]!@#$%^&*\\\'\">?<,."
+value_for_password = str()
+
+upper, lower, nums, symbs = True, True, True, True
+if upper: value_for_password += uppercase_letters
+if lower: value_for_password += lowercase_letters
+if nums: value_for_password += digits
+if symbs: value_for_password += symbols
+
+
 
 
